ebac32686abe145e2fd9afe092f6dc0eb6262262
Eric Mugo <mugoericpg20@gmail.com>
2023-07-05 12:27:20 +0300

    placeholder

---
+++ client/src/components/Footer.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Footer = () => {
+    return (
+        <footer className="bg-gray-900 text-white">
+
+        
+            <div className = "md:flex md:justify-between md:items_center sm:px-12">
+                <h1 className="lg:text-4x1 text-3x1 md:mb-0 mb-6 lg:leading-normal font-semibold md:w-2/5">
+                    <span className="text-teal-400">Free</span> until you're ready to launch
+                </h1>
+                <div>
+                    <input type="text" placeholder="Enter Your ph.no" className="text-gray-800
+                    sm:w-72 w-full sm:mr-5 mr-1 1g:mb-0 mb-4 py-2.5 rounded px-2 focus:outline-none"></input>
+                </div>
+
+                 
+
+
+
+            </div>
+        </footer>
+    )
+}
