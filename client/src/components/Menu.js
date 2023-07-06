978348388ef4ecacb704de5aee6bf09342f378c6
Eric Mugo <mugoericpg20@gmail.com>
2023-07-06 09:51:58 +0300

    menu

---
+++ client/src/components/Menu.js
@@ -0,0 +1,38 @@
+export const PRODUCTS = [
+    {name : 'Drag and Drop',link:'#'},
+    {name : 'Visual Studio X',link:'#'},
+    {name : 'Easy Content', link:'#'}
+]
+
+
+export const RESOURCES = [
+    {name:'Industries and tools',link:'#'},
+    {name:'use cases',link:'#'},
+    {name:'Blog',link:'#'},
+    {name:'Online event',link:'#'},
+    {name:'Nostrid exercitation',link:'#'},
+]
+
+export const  COMPANY = [
+    {name:'Diversity & inclusion',link:'#'},
+    {name:'About us',link:'#'},
+    {name:'Press',link:'#'},
+    {name:'Customer Stories',link:'#'},
+    {name:'Online communities',link:'#'},
+
+]
+
+export const SUPPORT = [
+    {name:'Documenatation',link:'#'},
+    {name:'Tutorials & guides', link:'#'},
+    {name:'Webinars',link:'#'},
+    {name:'Open-source',link:'#'},
+];
+
+export const Icons = [
+    {name:"logo-facebook",link:'#'},
+    {name:"logo-twitter",link:'#'},
+    {name:"logo-github",link:'#'},
+    {name:"logo-linkedin",link:'#'},
+    {name:"logo-instagram",link:'#'},
+]
