be26463a071c2ac094dfaf8ea0795749ed8c011e
Eric Mugo <mugoericpg20@gmail.com>
2023-07-06 09:47:47 +0300

    items

---
+++ client/src/components/ItemsContainer.js
@@ -0,0 +1,19 @@
+import Item from "./Item"
+
+
+const ItemsContainer = () => {
+    return 
+    <div className="grid grid-cols-1 sm:grid-cols-3 lg:grid-cols-4 gap-6 sm:ppx-8 px-5 py-16">
+       <Item />
+       <Item />
+       <Item />
+       <Item />
+       <Item />
+
+
+
+    </div>
+}
+ 
+
+export default ItemsContainer;
