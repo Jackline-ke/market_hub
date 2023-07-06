0a6f4dc50bfd5e44be216c8553854a804799e5b0
Jackline-ke <antoninahodhiambo@gmail.com>
2023-07-06 09:45:33 +0300

    file

---
+++ client/src/components/Banner.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Banner(){
+    return(
+        <div>
+            <div>
+                <h1>marketHub Title</h1>
+                <h2>marketHub CatchPhase</h2>
+                <button>Discover</button>
+            </div>
+            <div>
+                <img />
+            </div>
+        </div>
+    )
+}
+export default Banner;
