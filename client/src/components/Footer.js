908512ab8ad25e2856376cef9b4ddaff2ae7d806
Eric Mugo <mugoericpg20@gmail.com>
2023-07-06 10:40:10 +0300

    social icons

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ItemsContainer from './ItemsContainer';
+import SocialIcons from './Socialicons';
+import { Icons } from './Menu'; 
 
 
 const Footer = () => {
@@ -35,6 +37,8 @@ const Footer = () => {
             <ItemsContainer />
             <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-10 text-center pt-2 text-gray-400 text-sm pb-8">
                 <span>All rights reserved.</span>
+                <span>Terms  . Privacy Policy </span>
+                <SocialIcons Icons ={Icons} /> 
             </div>
         </footer>
     )
