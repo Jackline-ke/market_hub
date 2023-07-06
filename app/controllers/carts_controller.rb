bac28c35ff76c89855c06fb01b0f9f6c6246e504
sherry <sherry.kisilu@studebnt.moringaschool.com>
2023-07-06 11:17:08 +0300

    added controllers sk

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,2 +1,32 @@
+# app/controllers/carts_controller.rb
 class CartsController < ApplicationController
-end
+    before_action :set_buyer
+    before_action :set_cart, only: [:show, :update]
+  
+    def show
+      render json: @cart
+    end
+  
+    def update
+      if @cart.update(cart_params)
+        render json: @cart
+      else
+        render json: @cart.errors, status: :unprocessable_entity
+      end
+    end
+  
+    private
+  
+    def set_buyer
+      @buyer = Buyer.find(params[:buyer_id])
+    end
+  
+    def set_cart
+      @cart = @buyer.cart
+    end
+  
+    def cart_params
+      params.require(:cart).permit(:buyer_id)
+    end
+  end
+  
