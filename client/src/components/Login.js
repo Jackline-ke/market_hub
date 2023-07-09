83d894782419f4d21d3d7433cd377bbc28a54519
Eric Mugo <mugoericpg20@gmail.com>
2023-07-10 01:56:09 +0300

    login

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,2 +1,25 @@
 import React from "react";
+import loginImg from "client/src/assests/login.jpg"
+
+export default function Login() {
+    return (
+        <div className='grid grid-cols-1 sm:grid-cols-2'>
+            <div>
+                <img src={loginImg} alt=""></img>
+            </div>
+
+            <div>
+                <form>
+                    <h2>SIGN IN</h2>
+                    <div>
+                        <label>User Name</label>
+                    </div>
+                </form>
+           
+            </div>
+        </div>
+
+        
+    )
+}
 
